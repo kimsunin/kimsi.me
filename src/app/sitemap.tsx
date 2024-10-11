6cdc6bdfc04913df35a1c6836088815c3bf36a61
kimsunin <suninkim10@gmail.com>
2024-10-11 17:36:08 +0900

    fix:sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -14,11 +14,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 0.8,
     },
-    {
-      url: "https://kimsi.me/craft",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    },
   ];
 }
