a15d7c88b8ee81b1f5b56ef37dfee741040b55b5
kimsunin <suninkim10@gmail.com>
2024-11-05 19:24:11 +0900

    fix:NotoinRenderer code렌더링 안되는 오류 해결

--- src/component/_Blog/Renderer/Renderer.tsx
+++ src/component/_Blog/Renderer/Renderer.tsx
@@ -1,13 +1,68 @@
 "use client";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import Image from "next/image";
+import { useMemo } from "react";
+import { NotionComponents } from "react-notion-x";
 import { NotionRenderer } from "react-notion-x";
 import "react-notion-x/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+
+const Code = dynamic(() =>
+  import("react-notion-x/build/third-party/code").then(async (m) => {
+    await Promise.allSettled([
+      // @ts-ignore
+      import("prismjs/components/prism-markup.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-bash.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-diff.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-git.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-markup"),
+      // @ts-ignore
+      import("prismjs/components/prism-markdown.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-python.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-sql.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-yaml.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-typescript.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-css.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-javascript.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-json.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-jsx.js"),
+      // @ts-ignore
+      import("prismjs/components/prism-tsx.js"),
+    ]);
+    return m.Code;
+  })
+);
+
+const Collection = dynamic(() =>
+  import("react-notion-x/build/third-party/collection").then(
+    (m) => m.Collection
+  )
+);
 
 type PropsType = {
   post: any;
 };
 
 function Renderer({ post }: PropsType) {
-  return <NotionRenderer recordMap={post} />;
+  const components: Partial<NotionComponents> = useMemo(
+    () => ({ Code, nextImage: Image, nextLink: Link, Collection }),
+    []
+  );
+
+  return <NotionRenderer recordMap={post} components={components} />;
 }
 
 export default Renderer;
